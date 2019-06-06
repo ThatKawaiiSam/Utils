49d1c2127fa84953036113d56aa0f5ed2542f84c
ThatKawaiiSam <sam.sergi@outlook.com>
2019-06-07 09:26:21 +1000

    Added 1.14 support.

--- src/main/java/io/github/thatkawaiisam/utils/playerversion/PlayerVersion.java
+++ src/main/java/io/github/thatkawaiisam/utils/playerversion/PlayerVersion.java
@@ -13,7 +13,8 @@ public enum PlayerVersion {
     v1_10(210),
     v1_11(315, 316),
     v1_12(335, 338, 340),
-    v1_13(393, 401, 404);
+    v1_13(393, 401, 404),
+    v1_14(477, 480, 485);
 
     private Integer[] rawVersion;
 
