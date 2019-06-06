23d817cc929e8ff0784f14abc1258af4d839a980
ThatKawaiiSam <sam.sergi@outlook.com>
2019-06-07 09:25:15 +1000

    Ability to load and unload cached inventory.

--- src/main/java/io/github/thatkawaiisam/utils/CachedInventory.java
+++ src/main/java/io/github/thatkawaiisam/utils/CachedInventory.java
@@ -11,16 +11,31 @@ import java.util.UUID;
 public class CachedInventory {
 
     private final ItemStack[] cachedInventory, cachedArmor;
-    private final UUID inventoryUUID = UUID.randomUUID();
-    private final double health, food;
+    private final double health;
+    private final int totalExperience, food;
+    private final float exp;
 
-    /**
-     * Cached Inventory Class
-     */
     public CachedInventory(Player player) {
         cachedInventory = player.getInventory().getContents();
         cachedArmor = player.getInventory().getArmorContents();
         health = player.getHealth();
         food = player.getFoodLevel();
+        totalExperience = player.getTotalExperience();
+        exp = player.getExp();
+    }
+
+    //From player to cached inventory.
+    public static CachedInventory fromPlayer(Player player) {
+        return new CachedInventory(player);
+    }
+
+    //From Inventory to player.
+    public void applyToPlayer(Player player) {
+        player.getInventory().setContents(this.cachedInventory);
+        player.getInventory().setArmorContents(this.cachedArmor);
+        player.setHealth(this.health);
+        player.setFoodLevel(this.food);
+        player.setTotalExperience(this.totalExperience);
+        player.setExp(this.exp);
     }
 }
